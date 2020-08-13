4c447785b4482927c54646a8f54746eecd803b85
cjburgos21 <37281082+cjburgos21@users.noreply.github.com>
2020-08-13 12:28:01 -0600

    Introduccion a eventos y formularios

--- Aprender_ReactJS_Bases/my-app/src/App.js
+++ Aprender_ReactJS_Bases/my-app/src/App.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
-import cars from './data/cars.json'
-//import ConditionalSection from './sections/conditional'
 
-class CarItem extends Component {
-  render(){
-    const {car, id} = this.props
-
-    return (
-      <li>
-        <p>Key: {id}</p>
-        <p><strong>Nombre: </strong>{car.name}</p>
-        <p><strong>Marca: </strong>{car.company}</p>
-      </li>
-    )
-  }
-}
 
 class App extends Component {
-  render () {
+  constructor (){
+    super()
+    this.state = {mouseX: 0, mouseY: 0}
+    //Utilizando bind para que el contexto se enlace al metodo
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
 
+  handleMouseMove  = (e) => {
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+
+  handleClick (e){
+    console.log(e)
+    console.log(e.nativeEvent)
+      alert('Hola por aqui')
+  }
+  render () {
     return (
     <div className="App">
-      <h4>Trabajando con listas</h4>
-      <ul>
-        {
-          cars.map(car => {
-            return <CarItem id={car.id} key={car.id} car={car}/>
-          })
-        }
-      </ul>
-
+      <h4>Eventos
+      </h4>
+      <button onClick={this.handleClick}>Hola!</button>
+      <div
+        onMouseMove={this.handleMouseMove}
+        style={{ border: '1px solid #000', margintTop:10, padding:10 }}>
+        <p>{this.state.mouseX},{this.state.mouseY}</p>  
+      </div>
     </div>
   );
 
